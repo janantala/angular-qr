9dac206c50ca0c9ec9c40d24bcb61aee42f3b9dc
Jan Antala <jan@antala.sk>
2013-11-05 13:35:01 +0100

    Update grunt file - use ngmin, uglify, concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,67 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-ngmin');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    // Default task.
-    grunt.registerTask('default', ['karma', 'jshint']);
+  // Default task.
+  grunt.registerTask('default', ['karma', 'jshint']);
+  grunt.registerTask('build', ['karma', 'jshint', 'concat', 'ngmin', 'uglify']);
 
-    var karmaConfig = function(configFile, customOptions) {
-        var options = { configFile: configFile, keepalive: true };
-        var travisOptions = process.env.TRAVIS && { browsers: ['Firefox'], reporters: 'dots' };
-        return grunt.util._.extend(options, customOptions, travisOptions);
-    };
+  var karmaConfig = function(configFile, customOptions) {
+      var options = { configFile: configFile, keepalive: true };
+      var travisOptions = process.env.TRAVIS && { browsers: ['Firefox'], reporters: 'dots' };
+      return grunt.util._.extend(options, customOptions, travisOptions);
+  };
 
-    // Project configuration.
-    grunt.initConfig({
-        karma: {
-            unit: {
-            options: karmaConfig('test/test.conf.js')
-            }
-        },
-        jshint:{
-            files:['src/**/*.js', 'test/**/*.js', 'demo/js/controllers/todoCtrl.js'],
-            options: {
-                curly:true,
-                eqeqeq:true,
-                immed:true,
-                latedef:true,
-                newcap:true,
-                noarg:true,
-                sub:true,
-                boss:true,
-                eqnull:true,
-                devel:true,
-                globals:{}
-            }
+  // Project configuration.
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('bower.json'),
+      uglify: {
+        options: {
+          preserveComments: 'some'
+      },
+        dist: {
+          src: '<%= pkg.name %>.js',
+          dest: '<%= pkg.name %>.min.js'
         }
-    });
+      },
+      concat: {
+        options: {
+          process: true
+        },
+        dist: {
+          src: 'src/<%= pkg.name %>.js',
+          dest: '<%= pkg.name %>.js'
+        }
+      },
+      karma: {
+          unit: {
+          options: karmaConfig('test/test.conf.js')
+        }
+      },
+      jshint:{
+        files:['src/**/*.js', 'test/**/*.js', 'demo/**/*.js'],
+        options: {
+          curly:true,
+          eqeqeq:true,
+          immed:true,
+          latedef:true,
+          newcap:true,
+          noarg:true,
+          sub:true,
+          boss:true,
+          eqnull:true,
+          devel:true,
+          globals:{}
+        }
+      },
+      ngmin: {
+        dist: {
+          src: '<%= pkg.name %>.js',
+          dest: '<%= pkg.name %>.js'
+        }
+      }
+  });
 };
